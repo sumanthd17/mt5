cd25bb9d344bda1fe122e10cfc15e7eb82b08927
Sumanth Doddapaneni <dsumanth17@gmail.com>
2021-03-01 17:26:43 +0530

    update hi_en

--- multilingual_t5/hi_en/hi_en.py
+++ multilingual_t5/hi_en/hi_en.py
@@ -40,22 +40,37 @@ class HiEn(tfds.core.GeneratorBasedBuilder):
 
   def _split_generators(self, dl_manager: tfds.download.DownloadManager):
     """Returns SplitGenerators."""
-    # TODO(hi_en): Downloads the data and defines the splits
-    train = dl_manager.download_and_extract('https://storage.googleapis.com/ai4b-anuvaad-nmt/test-runs/train.zip')
-    val = dl_manager.download_and_extract('https://storage.googleapis.com/ai4b-anuvaad-nmt/test-runs/validation.zip')
+    hi = dl_manager.download_and_extract('https://anuvaad-parallel-corpus.s3-us-west-2.amazonaws.com/train-2021-v1-en-hi.zip')
+
+    devtest = dl_manager.download_and_extract('https://anuvaad-parallel-corpus.s3-us-west-2.amazonaws.com/devtest-2021-v1.zip')
 
     # TODO(hi_en): Returns the Dict[split names, Iterator[Key, Example]]
     return {
-        'train': self._generate_examples(source=train/'train.hi', target=train/'train.en'),
-        'validation': self._generate_examples(source=train/'validation.hi', target=train/'validation.en')
+        'train': self._generate_examples(source=hi/'en-hi/train.hi', target=hi/'en-hi/train.en'),
+        'validation': self._generate_examples(source=devtest/'devtest/all/en-hi/dev.hi', target=devtest/'devtest/all/en-hi/dev.en'),
+        'test': self._generate_examples(source=devtest/'devtest/all/en-hi/test.hi', target=devtest/'devtest/all/en-hi/test.en'),
     }
 
   def _generate_examples(self, source, target):
     """Yields examples."""
     # TODO(hi_en): Yields (key, example) tuples from the dataset
-    with open(source) as src, open(target) as tgt: 
-      for idx, row in zip(textfile1, textfile2):
-          yield idx, {
-            'source': row[0],
-            'target': row[1]
-          }
+    beam = tfds.core.lazy_imports.apache_beam
+
+    src = tf.io.gfile.GFile(bn_src, mode='r').readlines()
+    tgt = tf.io.gfile.GFile(bn_tgt, mode='r').readlines()
+
+    d = {}
+    d['src'] = src
+    d['tgt'] = tgt
+
+    def _process_file(d):
+      src = d['src']
+      tgt = d['tgt']
+
+      for idx, row in enumerate(zip(src, tgt)):
+        yield idx, {
+          'source': row[0],
+          'target': row[1]
+        }
+
+    return (beam.Create([d]) | beam.FlatMap(_process_file))
